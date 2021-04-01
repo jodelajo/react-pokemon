b7eca8b0032ddfddc44f7aa9a03ead4134318531
jodelajo <jodelajo@gmail.com>
2021-04-01 19:22:26 +0200

    lots

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Pokemon from "./components/Pokemon";
+import logo from "../src/assets/Pik.png"
+
+import axios from "axios";
+
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [pokemons, setPokemons] = useState([]);
+    const [clickForward, toggleClickForward] = useState(false)
+    const [clickBackward, toggleClickBackward] = useState(false)
+    const [error, setError] = useState('')
+    const [url, setUrl] = useState('')
+
+
+
+    console.log(clickForward)
+    function handleClickForward (url){
+        if (toggleClickForward) {
+
+        }
+
+
+
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+
+            setError('')
+            try {
+                const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
+                console.log(result.data.results);
+                console.log(result.data)
+                setPokemons(result.data.results);
+                toggleClickForward(!clickForward);
+                console.log(result.data.next)
+                let url = result.data
+                console.log(url)
+
+            } catch (e) {
+                setError("Er is iets misgegaan met het ophalen van de data.")
+                console.error(e)
+            }
+        }
+
+        fetchData();
+    }, [clickForward]);
+
+    return (
+        <>
+            <div className="header">
+    <img src={logo} alt="logo" className="logo"/>
+            </div>
+            <div className="button">
+            <button type="button" className="single-button" >Previous</button>
+            <button type="button" className="single-button" onClick={handleClickForward}>Next</button>
+            </div>
+
+            <div className="pokemon-container">
+                {error && <p>{error}</p>}
+
+                    {pokemons && pokemons.map((pokemon) => {
+                        return <Pokemon name={pokemon.name}/>
+
+                    })}
+
+            </div>
+
+            {/*<Pokemon name="ditto"/>*/}
+            {/*<Pokemon name="pikachu"/>*/}
+
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+function Pokemon({ name}){
+    const [pokemon, setPokemon] = useState([]);
+    const [pokemonAbilities, setPokemonAbilities] = useState([])
+    const [mooves, setMooves] = useState([])
+
+    useEffect(() =>{
+    async function fetchSinglePokemon(){
+        try{
+            const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+            // console.log(result.data)
+            setPokemon(result.data)
+            setPokemonAbilities(result.data.abilities)
+            setMooves(result.data.moves)
+            console.log(result.data.abilities)
+        } catch (e) {
+            console.error(e)
+        }
+
+    }
+
+fetchSinglePokemon();
+    },([]))
+
+    return (
+        <div className="pokemon-card">
+            <h2>{pokemon.name}</h2>
+            <img src={pokemon.sprites?.front_default} className="pokemon-image"/>
+            <p>Gewicht: {pokemon.weight}</p>
+            <p>Aantal Moves: {mooves.length}</p>
+            <h3> Lijst Abilities:</h3>
+
+            <ul>
+                {pokemonAbilities.map((pokemonAbility) =>{
+                    return <li key={pokemonAbility.ability.name}>{pokemonAbility.ability.name}</li>
+                })}
+            </ul>
+
+
+        </div>
+    );
+}
+export default Pokemon;
